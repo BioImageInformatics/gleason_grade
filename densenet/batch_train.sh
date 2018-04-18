a031e6cc99a86685b8e116aab1aa4ea9015655cc
nathan <ing.nathany@gmail.com>
2018-04-18 15:29:10 -0700

    edit change

--- densenet/batch_train.sh
+++ densenet/batch_train.sh
@@ -11,7 +11,7 @@ img_ratios=(0.25 0.5 1.0)
 crop_sizes=(1024 512 256)
 basedirs=('5x' '10x' '20x')
 
-for i in `seq 0 2`; do
+for i in `seq 1 2`; do
   for k in `seq 0 4`; do
     python ./train.py ${batch_sizes[$i]} ${img_ratios[$i]} ${crop_sizes[$i]} ${basedirs[$i]}
   done
