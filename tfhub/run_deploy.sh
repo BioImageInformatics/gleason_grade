d6ae6abd5805263e089f0fff51621d63016cfaf0
nathan <ing.nathany@gmail.com>
2018-05-29 17:46:18 -0700

    ignore inference folders

--- tfhub/run_deploy.sh
+++ tfhub/run_deploy.sh
@@ -2,20 +2,20 @@
 
 set -e
 
-# svs_dir=/media/nathan/d5fd9c1c-4512-4133-a14c-0eada5531282/slide_data/CEDARS_PRAD
-svs_dir=/media/nathan/d5fd9c1c-4512-4133-a14c-0eada5531282/slide_data/durham
-# svs_dir=/media/ing/D/svs/TCGA_PRAD
-model_name=inception_v3
-outdir="inference/$model_name"
-model_path="snapshots/$model_name"
+# svs_dir=/media/nathan/d5fd9c1c-4512-4133-a14c-0eada5531282/slide_data/durham
+svs_dir=../data/durham_validation
+
+model_names = (
+inception_v3
+nasnet_large
+mobilenet_v2_050_224
+resnet_v2_152
+)
 
 svslist=$( ls $svs_dir/*svs )
 
-for svs in ${svslist[@]}; do
- echo $svs $outdir
- # python ./deploy_retrained.py --model_path $model_path --slide $svs --out $outdir
+for model_name in ${model_names[@]}; do
+  outdir="inference/$model_name"
+  model_path="snapshots/$model_name"
+  python ./deploy_retrained.py --model_path $model_path --slide $svs_dir --out $outdir
 done
-
-python ./deploy_retrained.py --model_path $model_path --slide $svs_dir --out $outdir
-
-# ls $svs_dir/*svs | shuf | parallel --jobs 2 "python deploy_retrained.py --model_path=$model_path --out=$outdir --slide={}"
