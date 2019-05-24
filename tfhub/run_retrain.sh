2cb02415e153289ba42f4db4ad80726cbb1590dc
nathanin <ing.nathany@gmail.com>
2019-05-24 10:02:37 -0700

    .?

--- tfhub/run_retrain.sh
+++ tfhub/run_retrain.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 set -e
-# module_url=https://tfhub.dev/google/imagenet/inception_v3/feature_vector/1
 module_name=inception_v3
-# module_url="https://tfhub.dev/google/imagenet/$module_name/feature_vector/1"
 module_url="https://tfhub.dev/google/imagenet/$module_name/feature_vector/1"
 
 python retrain.py --image_dir ../data/tfhub_data \
@@ -11,6 +9,6 @@ python retrain.py --image_dir ../data/tfhub_data \
 --bottleneck_dir ./bottlenecks/$module_name \
 --tfhub_module $module_url \
 --saved_model_dir ./snapshots/$module_name \
---how_many_training_steps 1000 \
+--how_many_training_steps 5000 \
 --learning_rate 0.01 \
---flip_left_right 1
+--flip_left_right 0
