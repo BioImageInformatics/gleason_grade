8e2c10e5dc21e8069f15b59b1e974acc099fe0b0
nathan <ing.nathany@gmail.com>
2019-06-18 11:50:48 -0700

    Massive restructuring
    Move everything out of individual model def directories
    Expose Training and Inference classes under one package
    
    Allow import gleason_grade as gg ; gg.(stuff)
    
    Sort out tables with validation metrics into a new directory
    
    Sort out tools for use with new svsutils

---
+++ gleason_grade/__init__.py
@@ -0,0 +1,32 @@
+from .densenet import Training as DensenetTraining
+from .densenet import Inference as DensenetInference
+
+from .densenet_small import Training as DensenetSmallTraining
+from .densenet_small import Inference as DensenetSmallInference
+
+from .fcn8s import Training as FcnTraining
+from .fcn8s import Inference as FcnInference
+
+from .fcn8s_small import Training as FcnSmallTraining
+from .fcn8s_small import Inference as FcnSmallInference
+
+from .unet import Training as UnetTraining
+from .unet import Inference as UnetInference
+
+from .unet_small import Training as UnetSmallTraining
+from .unet_small import Inference as UnetSmallInference
+
+__all__ = [
+    'DensenetTraining',
+    'DensenetInference',
+    'DensenetSmallTraining',
+    'DensenetSmallInference',
+    'FcnTraining',
+    'FcnInference',
+    'FcnSmallTraining',
+    'FcnSmallInference',
+    'UnetTraining',
+    'UnetInference',
+    'UnetSmallTraining',
+    'UnetSmallInference'
+]
---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name=        "gleason_grade",
+    version=     "0.1",
+    author=      "Nathan Ing",
+    author_email="ing.nathany@gmail.com",
+    description ="Model defs training and inference scripts",
+    url         ="https://github.com/BioImageInformatics/gleason_grade",
+    packages    =setuptools.find_packages()
+)
